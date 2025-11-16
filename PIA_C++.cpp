936363454d1da20ab81877388b4027ea1f11219a
Eddy Barrios <eddybarrios2000@gmail.com>
2025-11-16 16:57:23 -0600

    menu principal

---
+++ PIA_C++.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+	int opc;
+	
+	do
+	{
+		cout << "\n----MENU----\n"
+		<< "a) REGISTRO DE VUELOS\n"
+		<< "b) LISTADO DE DESTINOS\n"
+		<< "c) LISTADO DE VUELOS \n"
+		<< "d) SALIR" << endl;
+		
+		cin >> opc;	
+		
+	}while(false);
+	
+	switch(opc)
+	{
+		case: 'a'
+			
+	}
+	
+	
+}
